2f96dff872a172b9098037ced761e4ce61e434ee
Vasavi <vasavi.uppada@sunbasket.com>
2017-08-01 13:24:07 -0700

    Adding Config

---
+++ Automation/src/main/java/com/SunBasket/Config/Config.java
@@ -0,0 +1,15 @@
+package com.SunBasket.Config;
+
+public class Config {
+
+	public static class url{
+		public static String base_url = "https://master.sunbasket-staging.com";
+//		public static String base_url = "https://sunbasket.com";
+	}
+	
+	public static class Browser{
+//		public static String browser = "firefox";
+		public static String browser = "chrome";
+	}
+	
+}
